5f14a7ed4f1affa040f35dcba043bb92303941fd
AmirK <amirkaramuja@gmail.com>
2019-08-08 13:38:02 -0400

    Fix component: Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, RouteProps, Redirect, withRouter } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import PokemonListPage from './components/PokemonListPage';
-import About from './components/About';
-import PokemonPage from './components/PokemonPage';
-import ItemPage from './components/ItemPage';
-import ItemListPage from './components/ItemListPage';
-import FavoritesPage from './components/FavoritesPage';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  RouteProps,
+  Redirect,
+  withRouter,
+} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import PokemonListPage from "./components/PokemonListPage";
+import About from "./components/About";
+import PokemonPage from "./components/PokemonPage";
+import ItemPage from "./components/ItemPage";
+import ItemListPage from "./components/ItemListPage";
+import FavoritesPage from "./components/FavoritesPage";
 
-interface IPrivateRouteProps extends RouteProps {}
+interface IPrivateRouteProps extends RouteProps {
+  component: any;
+}
 
 const authentication = {
   isAuthenticated: false,
@@ -28,7 +37,9 @@ const App: React.FC = () => {
   return (
     <Router>
       <div>
-        <Navbar links={['Home', 'Pokemon', 'Items', 'Favorites', 'About', 'Login']}>
+        <Navbar
+          links={["Home", "Pokemon", "Items", "Favorites", "About", "Login"]}
+        >
           <AuthButton />
         </Navbar>
         <Switch>
@@ -60,7 +71,7 @@ class Login extends React.Component<any, {}> {
   };
 
   render() {
-    const { from } = this.props.location.state || { from: { pathname: '/' } };
+    const { from } = this.props.location.state || { from: { pathname: "/" } };
     const { redirectToReferrer } = this.state;
 
     if (redirectToReferrer === true) return <Redirect to={from} />;
@@ -75,8 +86,7 @@ class Login extends React.Component<any, {}> {
 }
 
 const PrivateRoute = (props: IPrivateRouteProps) => {
-  const { component, ...rest } = props;
-
+  const { component: Component, ...rest } = props;
   return (
     <Route
       {...rest}
@@ -86,33 +96,33 @@ const PrivateRoute = (props: IPrivateRouteProps) => {
         ) : (
           <Redirect
             to={{
-              pathname: '/login',
+              pathname: "/login",
               state: { from: props.location },
             }}
           />
-        )}
+        )
+      }
     />
   );
 };
 
-const AuthButton = withRouter(
-  ({ history }) =>
-    authentication.isAuthenticated ? (
-      <p>
-        Welcome!
-        <button
-          onClick={() => {
-            authentication.signout(() => history.push('/'));
-          }}
-          type="button"
-          className="btn btn-danger"
-        >
-          Sign out
-        </button>
-      </p>
-    ) : (
-      <p>You are not logged in</p>
-    ),
+const AuthButton = withRouter(({ history }) =>
+  authentication.isAuthenticated ? (
+    <p>
+      Welcome!
+      <button
+        onClick={() => {
+          authentication.signout(() => history.push("/"));
+        }}
+        type="button"
+        className="btn btn-danger"
+      >
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <p>You are not logged in</p>
+  ),
 );
 
 export default App;
